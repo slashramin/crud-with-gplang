792a619ed42bfc1776e5ca92c50998cf5cfc9af7
slashramin <moradi.ramin@hotmail.com>
2020-02-11 15:50:33 +0330

    sd

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -3,11 +3,11 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/example/simple-REST/pkg/utils"
+	"github.com/slashramin/crud-with-gplang/tree/master/pkg/utils"
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
-	"github.com/example/simple-REST/pkg/models"
+	"github.com/slashramin/crud-with-gplang/tree/master/pkg/models"
 )
 
 var NewBook models.Book
